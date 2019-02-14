3367dff474ded7b7ff037de5530d4b57e131ea64
jswz72 <jacobdasword@gmail.com>
2019-02-14 18:14:02 -0500

    init

---
+++ make_graph.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+vectorfile = 'glove.6B.50d.txt'
+graphfile = 'list.txt'
+LIMIT = 1000
+THRESHOLD = 3
+
+def euclidean_dist(x, y):
+    return np.sqrt(np.sum((np.subtract(x, y)) ** 2))
+
+with open (vectorfile) as vf:
+    mat = [line.split(' ') for line in vf.readlines()[:LIMIT]]
+    words = { row[0]: tuple([float(num) for num in row[1:]]) for row in mat }
+
+gf = open(graphfile, 'w')
+#retarr = []
+matches = set()
+for vtx1, val in words.items():
+    for vtx2, val2 in words.items():
+        if vtx1 == vtx2 or (vtx2, vtx) in matches:
+            continue
+        matches.add((vtx, vtx2))
+        dist = euclidean_dist(val, val2)
+        if dist > THRESHOLD:
+            continue
+        gf.write(f'{key} {key2} {dist}\n')
+        #retarr.append((key, key2, dist))
+
+# retarr.sort(key=lambda x: x[2])
+# print('Most related:')
+# for els in retarr[:10]:
+#     print(els)
+
+# print('\nLeast related')
+# for els in retarr[-10:]:
+#     print(els)
