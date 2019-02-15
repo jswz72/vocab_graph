ec439bca9f02bc6f523eb75a79d7b755bf519233
jswz72 <jacobdasword@gmail.com>
2019-02-15 12:07:46 -0500

    put into functions

--- make_graph.py
+++ make_graph.py
@@ -1,36 +1,70 @@
 import numpy as np
+import time
 
 vectorfile = 'glove.6B.50d.txt'
 graphfile = 'list.txt'
-LIMIT = 1000
+LIMIT = 10000
 THRESHOLD = 3
+
 
 def euclidean_dist(x, y):
     return np.sqrt(np.sum((np.subtract(x, y)) ** 2))
 
-with open (vectorfile) as vf:
-    mat = [line.split(' ') for line in vf.readlines()[:LIMIT]]
-    words = { row[0]: tuple([float(num) for num in row[1:]]) for row in mat }
+def collect_vectors(f):
+    mat = [line.split(' ') for line in f.readlines()[:LIMIT]]
+    return { row[0]: tuple([float(num) for num in row[1:]]) for row in mat }
 
-gf = open(graphfile, 'w')
-#retarr = []
-matches = set()
-for vtx1, val in words.items():
-    for vtx2, val2 in words.items():
-        if vtx1 == vtx2 or (vtx2, vtx1) in matches:
-            continue
-        matches.add((vtx1, vtx2))
-        dist = euclidean_dist(val, val2)
-        if dist > THRESHOLD:
-            continue
-        gf.write(f'{vtx1} {vtx2} {dist}\n')
-        #retarr.append((key, key2, dist))
+def parse_vector_file(filename):
+    words = None
+    with open (filename, encoding="utf8") as vector_file:
+        words = collect_vectors(vector_file)
+    return words
 
-# retarr.sort(key=lambda x: x[2])
-# print('Most related:')
-# for els in retarr[:10]:
-#     print(els)
+def find_edges(words, add_func, threshold=None):
+    for vtx1, vec1 in words.items():
+        for vtx2, vec2 in words.items():
+            if vtx1 == vtx2:
+                continue
+            dist = euclidean_dist(vec1, vec2)
+            if threshold and dist > threshold:
+                continue
+            add_func((vtx1, vtx2, dist))
 
-# print('\nLeast related')
-# for els in retarr[-10:]:
-#     print(els)
+
+def write_edge_list(words, outfile):
+    '''write edgelist to file at given path'''
+    of = open(outfile, 'w')
+    def add_func(edge_tuple):
+        vtx1, vtx2, dist = edge_tuple
+        of.write(f'{vtx1} {vtx2} {dist}')
+    find_edges(words, add_func)
+    of.close()
+
+def create_edge_list(words):
+    '''create and return edgelist in memory'''
+    returnarr = []
+    def add_func(edge_tuple):
+        returnarr.append(edge_tuple)
+    find_edges(words, add_func)
+    return returnarr
+
+    
+def sort_and_print_edges(edges):
+    edges.sort(key=lambda x: x[2])
+    print('Most related:')
+    for els in edges[:10]:
+        print(els)
+
+    print('\nLeast related')
+    for els in edges[-10:]:
+        print(els)
+
+start = time.time()
+words = parse_vector_file(vectorfile)
+parse_time = time.time()
+print(f'Time to parse vectorfile: {parse_time - start}')
+# edges = create_edge_list(words)
+write_edge_list(words, graphfile)
+edge_time = time.time()
+print(f'Time to create edges: {edge_time - parse_time}')
+#sort_and_print_edges(edges)
