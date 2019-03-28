a6201cac793d7b3d0e019d30a45d07d390798ea6
jswz72 <jacobdasword@gmail.com>
2019-03-28 19:56:39 -0400

    read source words from file

--- src/graph_ops/recommend.cpp
+++ src/graph_ops/recommend.cpp
@@ -14,7 +14,7 @@ using std::string;
 
 int main(int argc, char **argv) {
 	if (argc < 5) {
-		cout << "Input: ./exe base_file mapping_file num_recs source_words..." << endl;
+		cout << "Input: ./exe base_file mapping_file num_recs source_word_file rec_pool_file" << endl;
 		return 1;
 	}
 	
@@ -25,6 +25,8 @@ int main(int argc, char **argv) {
 
 	const char *mapping_file = argv[2];
 	int num_recs = atoi(argv[3]);
+	const char *source_word_file = argv[4];
+	const char *rec_pool_file = argv[5];
 	
 	graph<long, long, double, long, long, double> *csr = 
 		new graph <long, long, double, long, long, double>
@@ -33,20 +35,23 @@ int main(int argc, char **argv) {
 	std::cout << "Edges: " << csr->edge_count << std::endl;
     std::cout << "Verticies: " << csr->vert_count << std::endl;
 
-	int num_source_words = argc - 4;
 	std::vector<int> source_word_idxs;
 	std::vector<string> words = Utils::get_word_mapping(mapping_file);
 
-    for (int i = 0; i < num_source_words; i++) {
-		const char *source_word = argv[i + 4];
-		auto it = std::find(words.begin(), words.end(), source_word);
+    std::ifstream infile(source_word_file);
+    string source_word;
+    int counter = 0;
+    while (std::getline(infile, source_word)) {
+        auto it = std::find(words.begin(), words.end(), source_word);
 		if (it == words.end()) {
 			cout << "Not found in graph: " << source_word << endl;
 			return 1;
 		}
 		int idx = std::distance(words.begin(), it);
 		source_word_idxs.push_back(idx);
+        counter++;
     }
+	int num_source_words = counter;
 
 	std::vector<WordDist*> closest_words = ReviewAndRec::recommend(csr, source_word_idxs, num_recs);
 	cout << "\nLearning recommendations :" << endl;
