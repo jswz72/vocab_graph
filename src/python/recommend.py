353139fe1758117768aa1a8863efc3f49b3ff1bb
jswz72 <jacobdasword@gmail.com>
2019-03-31 19:33:19 -0400

    update test to 15 source words

--- src/python/recommend.py
+++ src/python/recommend.py
@@ -5,7 +5,7 @@ word_map_fname = '../cpp/get_edges/word-order.txt'
 
 graph = knowledge_graph.GraphOps(edge_list_fname, word_map_fname)
 
-source_words = ['flame', 'shark']
+source_words = ['flame', 'shark', 'cat', 'dog', 'brooklyn', 'berth', 'cherry', 'of', 'the', 'should', 'last', 'years', 'free', 'pair', 'today']
 res = graph.recommend(source_words, 3)
 for word in res:
     print(word)
