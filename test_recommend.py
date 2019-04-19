13f00249921d577da96f8c666cc89f38d2055d1c
jswz72 <jacobdasword@gmail.com>
2019-04-19 11:51:16 -0400

    separate group test case

--- test_recommend.py
+++ test_recommend.py
@@ -7,15 +7,9 @@ word_map_fname = './data/word-order.txt'
 graph = vocab_graph.VocabGraph(edge_list_fname, word_map_fname)
 
 source_words = ['flame', 'shark']
-print('REGULAR RECOMMENDATION: ')
 res = graph.recommend(source_words, 20)
-print("PYTHON:")
+print('Recommendations:')
 for word in res:
     print(word)
-
-print('GROUP RECOMMENDATION: ')
-groups = ['the', 'of', 'other', 'learn'], ['whale', 'torch', 'as'], ['flame', 'shark']
-group = graph.recommend_group(source_words, groups)
-print(group)
 
 
