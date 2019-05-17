a227a4830c6f6329f7513d14da7d27ea0fc974f0
Jacob Sword <jacobdasword@gmail.com>
2019-05-17 14:36:22 -0400

    rm unnecessary import

--- test_recommend_pronunciation.py
+++ test_recommend_pronunciation.py
@@ -1,5 +1,4 @@
 import vocab_graph
-import fuzzy
 import sys
 
 word1 = sys.argv[1]
@@ -8,7 +7,6 @@ word2 = sys.argv[2]
 edge_list_fname = './data/edge-list.txt'
 word_map_fname = './data/word-order.txt'
 
-dmetaphone = fuzzy.DMetaphone()
 graph = vocab_graph.VocabGraph(edge_list_fname, word_map_fname)
 
 recommendations = graph.recommend_phonetic([word1, word2], 3)
